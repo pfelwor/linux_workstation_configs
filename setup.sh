9f54df553775b8370050a7c10a5f87c2e70fbe66
Philipp Felwor <philipp.felwor@dpd.de>
2019-03-29 21:00:07 +0100

    fixed typos and wrong quotes

--- setup.sh
+++ setup.sh
@@ -18,11 +18,11 @@ echo "Please enter your mailaddress you want to be used for git:"
 read mailaddress
 echo "what name should be used for git?:"
 read name
-sed 's/MAILADDRESS/${mailaddress}/g' ./.gitconfig > ./.gitconfig_mail
+sed "s/MAILADDRESS/${mailaddress}/g" ./.gitconfig > ./.gitconfig_mail
 if [ $? -ne 0 ] ; then
     die 3
 fi
-sed 's/NAME/${name}/g' ./.gitconfig_mail > {HOME}/.gitconfig
+sed "s/NAME/${name}/g" ./.gitconfig_mail > ${HOME}/.gitconfig
 if [ $? -ne 0 ] ; then
     die 4
 fi

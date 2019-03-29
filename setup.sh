941de0699f4709726b2647ffd5118d59b378a842
Philipp Felwor <git@felwor.de>
2019-03-29 20:41:34 +0100

    refactored setup-script

--- setup.sh
+++ setup.sh
@@ -2,10 +2,34 @@
 # Script to deploy config-files into your home-dir
 
 echo "copying files into your home-directory"
-cp ./.bashrc ./.vimrc ./.gitconfig ./.kubectl_autocompletion ${HOME}/
-if [ $? -ne 0 ]
-then
-    echo "something went wrong - sorry :-("
-    exit 1
+cp ./.vimrc ./.kubectl_autocompletion ${HOME}/
+if [ $? -ne 0 ] ; then
+    die 1
 fi
+
+# expanding your .bashrc-file
+cat ./.bashrc >> ${HOME}/.bashrc
+if [ $? -ne 0 ] ; then
+    die 2
+fi
+
+# copying .gitconfig-file
+echo "Please enter your mailaddress you want to be used for git:"
+read mailaddress
+echo "what name should be used for git?:"
+read name
+sed 's/MAILADDRESS/${mailaddress}/g' ./.gitconfig > ./.gitconfig_mail
+if [ $? -ne 0 ] ; then
+    die 3
+fi
+sed 's/NAME/${name}/g' ./.gitconfig_mail > {HOME}/.gitconfig
+if [ $? -ne 0 ] ; then
+    die 4
+fi
+
+function die {
+    echo "something went wrong - sorry :-("
+    exit $1
+}
+
 exit 0
